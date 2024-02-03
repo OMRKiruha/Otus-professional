b6bb519134b66d8782f89152a1a1a96322b67502
OMRKiruha <omr-kiruha@yandex.ru>
2024-02-03 21:07:33 +0400

    ДЗ03 Аллокатор работает в стандартном контейнере

--- hw03/MyAllocator.h
+++ hw03/MyAllocator.h
@@ -4,3 +4,38 @@
 
 #pragma once
 
+#include <cstddef>
+
+
+template<typename T>
+struct MyAllocator {
+    using value_type = T;
+
+    MyAllocator() noexcept = default;
+
+    ~MyAllocator() = default;
+
+    template<class U>
+    explicit MyAllocator(const MyAllocator<U> &) noexcept {}
+
+    T *allocate(size_t n) {
+        return static_cast<T *>(::operator new(n * sizeof(T)));
+    }
+
+    void deallocate(T *p, [[maybe_unused]]size_t n) {
+        ::operator delete(p);
+    }
+
+};
+
+template <class T, class U>
+constexpr bool operator== (const MyAllocator<T>& a1, const MyAllocator<U>& a2) noexcept
+{
+    return true;
+}
+
+template <class T, class U>
+constexpr bool operator!= (const MyAllocator<T>& a1, const MyAllocator<U>& a2) noexcept
+{
+    return false;
+}
--- hw03/main.cpp
+++ hw03/main.cpp
@@ -4,8 +4,9 @@
 #include <cstdint>
 #include <iostream>
 #include <map>
+#include <forward_list>
 
-#include "MyForwardList.h"
+#include "MyVector.h"
 #include "MyAllocator.h"
 
 
@@ -42,7 +43,7 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char const *argv[]) {
     fillByFactorial(std_allocator_map, 10);
 
     // создание  экземпляра  std::map<int, int> с новым аллокатором, ограниченным 10 элементами
-    std::map<int32_t, int32_t, std::less<>, std::allocator<std::pair<int32_t, int32_t>>> my_allocator_map;
+    std::map<int32_t, int32_t, std::less<>, MyAllocator<std::pair<int32_t, int32_t>>> my_allocator_map;
 
     // заполнение 10 элементами, где ключ - это число от 0 до 9, а значение - факториал ключа
     fillByFactorial(my_allocator_map, 10);
@@ -52,20 +53,22 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char const *argv[]) {
     printContainer(std_allocator_map, "std::map with custom allocator");
 
     // создание экземпляра своего контейнера для хранения значений типа int
-    MyForwardList<int32_t> std_allocator_forward_list;
+    MyVector<int32_t> std_allocator_vector;
 
     // заполнение 10 элементами от 0 до 9
-    fillBySequence(std_allocator_forward_list, 0, 9);
+    fillBySequence(std_allocator_vector, 0, 9);
 
     // создание экземпляра своего контейнера для хранения значений типа int с новым аллокатором, ограниченным 10 элементами
-    MyForwardList<int32_t> my_allocator_forward_list;
+    MyVector<int32_t> my_allocator_vector;
 
     // заполнение 10 элементами от 0 до 9
-    fillBySequence(my_allocator_forward_list, 0, 9);
+    fillBySequence(my_allocator_vector, 0, 9);
 
     // вывод на экран всех значений, хранящихся в контейнере
-    printContainer(std_allocator_forward_list, "MyForwardList with std::allocator");
-    printContainer(my_allocator_forward_list, "MyForwardList with std::allocator");
+    printContainer(std_allocator_vector, "MyForwardList with std::allocator");
+    printContainer(my_allocator_vector, "MyForwardList with std::allocator");
+
+    std::forward_list<int> fl;
 
     return 0;
 }
